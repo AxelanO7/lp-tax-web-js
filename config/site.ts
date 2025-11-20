f4e5315d37d7c087a766784f43c27327188ed7f8
AxelanO7 <jeremia123.jm@gmail.com>
2025-11-20 17:40:02 +0800

    ✨ chore(site): update site name and description
    - update site name to "Pajak Kita"
    - update site description to "Pajak Kita, Solusi Pajak untuk Anda"
    - update favicon

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Pajak Kita",
+  description: "Pajak Kita, Solusi Pajak untuk Anda",
   navItems: [
     {
       label: "Home",
